83142f452e3a26f93681135a14f195e7e0f86d8f
JongHo <jhleed@naver.com>
2017-08-29 23:02:18 +0900

    init project

---
+++ src/main/java/me/james/simplemessageserver/SimpleMessageServerApplication.java
@@ -0,0 +1,12 @@
+package me.james.simplemessageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMessageServerApplication.class, args);
+    }
+}
---
+++ src/test/java/me/james/simplemessageserver/SimpleMessageServerApplicationTests.java
@@ -0,0 +1,16 @@
+package me.james.simplemessageserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleMessageServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
